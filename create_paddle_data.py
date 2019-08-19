488c2f101569b252121244cae5d70c2c01e752e5
zhhsplendid <zhhsplendid@163.com>
2019-08-19 11:59:50 +0000

    Add multiple GPU cards test

--- create_paddle_data.py
+++ create_paddle_data.py
@@ -7,13 +7,14 @@ np.random.seed(123)
 ONE_G = 2**30
 
 # float32 takes 4 bytes. So 2.5 * ONE_G float32 constant will take 10 GiB
-x = fluid.layers.fill_constant(shape=[1, 2.5 * ONE_G],
+x = fluid.layers.fill_constant(shape=[4, 2.5 * ONE_G / 4],
                                value=1,
                                dtype='float32')
 y = fluid.layers.reduce_max(x, dim=1)
 
+compiled_prog = fluid.CompiledProgram(fluid.default_main_program()).with_data_parallel()
 exe = fluid.Executor(core.CUDAPlace(0))
-output = exe.run(program=fluid.default_main_program(), fetch_list=[y])
+output = exe.run(program=compiled_prog, fetch_list=[y])
 
-print("output = %f" % output[0])
+print("output = %f" % output[0][0])
 print("Create data on GPU successfully")
